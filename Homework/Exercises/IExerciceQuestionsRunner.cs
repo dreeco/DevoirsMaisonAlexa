efd9b9eed082fd4d4f45b6b416bc8751838b0ae7
dreeco <adrisheep@hotmail.com>
2024-09-27 11:29:56 +0200

    delete useless code

--- Homework/Exercises/IExerciceQuestionsRunner.cs
+++
@@ -1,9 +0,0 @@
-﻿using Homework.Models;
-
-namespace Homework.HomeworkExercises;
-
-public interface IExerciceQuestionsRunner
-{
-    Question NextQuestion(int age, IEnumerable<string> alreadyAsked);
-    AnswerValidation ValidateAnswer(string questionKey, string answer);
-}
